1e409f52ebdd8b0dde90595b991059357666cbe3
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-08 16:40:30 +0545

    feat(service-workers): Creating a Service Worker

--- service-worker/web/js/sw.js
+++ service-worker/web/js/sw.js
@@ -1,4 +1,23 @@
 'use strict';
 
-// TODO
+const version = 1;
 
+self.addEventListener('install', onInstall);
+self.addEventListener('activate', onActivate);
+
+main().catch(console.error);
+
+// ******************************************
+
+async function main() {
+  console.log(`[Service Worker] (${version}) is starting...`);
+}
+
+async function onInstall(event) {
+  console.log(`[Service Worker] (${version}) installed.`);
+  self.skipWaiting();
+}
+
+async function onActivate(event) {
+  console.log(`[Service Worker] (${version}) activated.`);
+}
